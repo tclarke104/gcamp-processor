99bc33c9611db468b9900eb51b44f8f20950adb7
Travis Clarke <travisjonathanclarke@gmail.com>
2019-04-14 20:08:17 -0400

    ready for andrew

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import numpy as np
-from matplotlib import pyplot as plt
-from tqdm import tqdm
 import cv2
 from scipy.ndimage import gaussian_filter
+from matplotlib import pyplot as plt
 from skimage import img_as_float
 from skimage.morphology import reconstruction
 from skimage.morphology import dilation, disk
@@ -10,99 +9,114 @@ from skimage.filters import threshold_triangle
 import xlsxwriter
 from skimage.measure import label, regionprops
 from skimage.morphology import remove_small_objects
-from skimage.color import label2rgb
-import matplotlib.patches as mpatches
 from tkinter import Tk
-from tkinter.filedialog import askopenfilename, askdirectory
+from tkinter.filedialog import askopenfilename
 # from pyfnnd import deconvolve
 
 Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
 filename = askopenfilename(title='Select a file to process')
-dirname = askdirectory(initialdir="~", title='Please select a directory')
 
+# init video capture
 cap = cv2.VideoCapture(filename)
+
+# read first frame to get properties
 read, image = cap.read()
 
-width = image.shape[1]
-height = image.shape[0]
-channels = image.shape[2]
-length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-
+# convert first frame to gray scale and initialize accumulator for max image
 max_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-progress_bar = tqdm(total=length+1)
 
+print('Reading in the video to get location of neurons')
+frame_number = 0
+# loop through all the frames in the video
 while True:
+    # read frame and whether or not it read a frame from the video
     ret, frame = cap.read()
     if ret:
+        # convert current frame to grayscale
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # take the maximum picture values from the accumlated max image and the current image
         max_image = np.maximum(frame, max_image)
+        frame_number += 1
     else:
+        print(f'Captured {frame_number} frames')
         break
-    progress_bar.update(1)
 
+print('Max image created, isolating neurons and neuropils')
+# convert image values from int8 to float
 image = img_as_float(max_image)
+# blur the image
 image = gaussian_filter(image, 1)
 
+# create seed points in image
 seed = np.copy(image)
 seed[1:-1, 1:-1] = image.min()
 mask = image
 
+# run dilation reconstruction on image. This contains the background.
 dilated = reconstruction(seed, mask, method='dilation')
 
+# subtract the dilated background image from the image
 subtracted = image - dilated
 
+
+# init disk kernel for dialtion
 selem = disk(3)
+# dilate the subtracted image
 dilated = dilation(subtracted, selem)
 
+# perform threshold on the image using triangle
 thresh = threshold_triangle(dilated)
 binary = dilated > thresh
 
+# label discrete objects and assign each labeled area a number.
 label_image = label(binary)
+# remove small (artifactual) objects from the image
 label_image = remove_small_objects(label_image, 300)
-image_label_overlay = label2rgb(label_image, image=image)
 
-fig, ax = plt.subplots(figsize=(10, 6))
-ax.imshow(image, cmap='gray')
-for i, region in enumerate(regionprops(label_image)):
-    minr, minc, maxr, maxc = region.bbox
-    rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
-                              fill=False, edgecolor='red', linewidth=1)
-
-    center_r = minr + (maxr - minr) / 2
-    center_c = minc + (maxc - minc) / 2
-
-    ax.add_patch(rect)
-    ax.annotate(i, (center_c, center_r), color='red', weight='bold',
-                fontsize=6, ha='center', va='center')
+# init disk kernel for dilationg to get neuropils
 kernel = disk(45)
 
+# perform dilation on the image to get the neuron and the surrounding neuropil
 neuropils = dilation(label_image, kernel)
+# subtract neuron from dilation to get just the neuron
 neuropils = neuropils - label_image
-plt.imshow(neuropils)
-plt.show()
 
-cap = cv2.VideoCapture('gcamp.avi')
+print('neuropils and neurons identified')
 
+print('recapturing video')
+# capture the video again
+cap = cv2.VideoCapture(filename)
+
+# init dictionary for neurons and the neurons with background subtraction
 neurons = {}
 neurons_subtracted = {}
+frame_number = 0
 
 while True:
     ret, frame = cap.read()
     if ret:
+        # convert current frame to grayscale
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame = frame.astype(float)
+        # get the properties of the labeled regions
         neuropil_regions = regionprops(neuropils, frame)
         for i, region in enumerate(regionprops(label_image, frame)):
-            if i not in neurons.keys():
-                neurons[i] = np.array([])
-                neurons_subtracted[i] = np.array([])
-            neurons[i] = np.append(neurons[i], [region.max_intensity])
-            neurons_subtracted[i] = np.append(neurons_subtracted[i], [(region.max_intensity - neuropil_regions[i].min_intensity)])
+            key = f'Neuron {i}'
+            if key not in neurons.keys():
+                neurons[key] = np.array([])
+                neurons_subtracted[key] = np.array([])
+            neurons[key] = np.append(neurons[key], [region.max_intensity])
+            neurons_subtracted[key] = np.append(neurons_subtracted[key], [(region.max_intensity - neuropil_regions[i].min_intensity)])
+        frame_number += 1
     else:
+        print(f'Captured {frame_number} frames')
         break
 
-
+# init dictionary for dF_f
 neurons_df_f = {}
 # neurons_deconv = {}
+plt.plot(neurons['Neuron 0'])
+plt.show()
 
 for i, neuron in enumerate(neurons.keys()):
     baseline = np.mean(neurons[neuron][100:200])
@@ -111,23 +125,26 @@ for i, neuron in enumerate(neurons.keys()):
     # n_best, c_best, LL, theta_best = deconvolve(np.array(df_f), dt=0.1, verbosity=0, learn_theta=(0, 1, 1, 1, 0) )
     # neurons_deconv[neuron] = n_best
 
-workbook = xlsxwriter.Workbook(f'{filename}-results.xlsx')
+out_file = f'{filename}-results.xlsx'
+print(f'writing results out to {out_file}')
+
+workbook = xlsxwriter.Workbook(out_file)
 raw_worksheet = workbook.add_worksheet('Raw')
 subtracted_worksheet = workbook.add_worksheet('Subtracted')
 df_f_worksheet = workbook.add_worksheet('DF_F')
 # deconvolved_worksheet = workbook.add_worksheet('Deconvolved')
 
 
-raw_worksheet.write_row(map(lambda num: f'Neuron {num}', neurons.keys()))
-subtracted_worksheet.write_row(map(lambda num: f'Neuron {num}', neurons.keys()))
-df_f_worksheet.write_row(map(lambda num: f'Neuron {num}', neurons.keys()))
+
+raw_worksheet.write_row(0,0,neurons.keys())
+subtracted_worksheet.write_row(0,0,neurons.keys())
+df_f_worksheet.write_row(0,0,neurons.keys())
 # deconvolved_worksheet.write_row(map(lambda num: f'Neuron {num}', neurons.keys()))
 
-numFrames = len(neurons[0])
-for neuron in neurons.keys():
-    raw_worksheet.write_column(1, neuron, neurons[neuron])
-    subtracted_worksheet.write_column(1, neuron, neurons_subtracted[neuron])
-    df_f_worksheet.write_column(1, neuron, neurons_df_f[neuron])
+for i, neuron in enumerate(neurons.keys()):
+    raw_worksheet.write_column(1, i, neurons[neuron])
+    subtracted_worksheet.write_column(1, i, neurons_subtracted[neuron])
+    df_f_worksheet.write_column(1, i, neurons_df_f[neuron])
     # deconvolved_worksheet.write_column(1, neuron, neurons_deconv[neuron])
 
 workbook.close()
